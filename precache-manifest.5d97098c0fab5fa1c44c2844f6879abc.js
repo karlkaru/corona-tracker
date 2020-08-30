f6cabde926627e6fe323d68067ff41e22cc6d615
karlkaru <67999912+karlkaru@users.noreply.github.com>
2020-08-30 23:25:49 +0300

    Updates

---
+++ precache-manifest.5d97098c0fab5fa1c44c2844f6879abc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe74a4a8123098ea4205f9ac62fc689c",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "1bc83145bb94f83db9e7",
+    "url": "/corona-tracker/static/css/main.00c47016.chunk.css"
+  },
+  {
+    "revision": "953fa04c009897248b8f",
+    "url": "/corona-tracker/static/js/2.ad2f3de1.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/corona-tracker/static/js/2.ad2f3de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc83145bb94f83db9e7",
+    "url": "/corona-tracker/static/js/main.da44ca19.chunk.js"
+  },
+  {
+    "revision": "e509bcded12a24e7dc1d",
+    "url": "/corona-tracker/static/js/runtime-main.61c4717e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
